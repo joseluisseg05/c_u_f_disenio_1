d7e6b61318102c8c9893c5d2fcca0974c6f2b0da
Jose Luis Garcia M <jl.garcia@ardana.com.mx>
2020-09-29 10:43:41 -0500

    Repeticion de animacion

---
+++ lib/src/pages/animacion_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as Math;
+
+class AnimacionPage extends StatelessWidget {
+  const AnimacionPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CuadradoAnimado(),
+      ),
+    );
+  }
+}
+
+class CuadradoAnimado extends StatefulWidget {
+  @override
+  _CuadradoAnimadoState createState() => _CuadradoAnimadoState();
+}
+
+class _CuadradoAnimadoState extends State<CuadradoAnimado>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> rotacion;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 4000),
+    );
+    rotacion = Tween(begin: 0.0, end: 2.0 * Math.pi).animate(controller);
+    controller.addListener(() {
+      if (controller.status == AnimationStatus.completed) {
+        controller.reverse();
+      }
+      /*else if (controller.status == AnimationStatus.dismissed) {
+        controller.forward();
+      }*/
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    controller.forward();
+
+    return AnimatedBuilder(
+      animation: controller,
+      child: _Rectangulo(),
+      builder: (BuildContext context, Widget child) {
+        return Transform.rotate(angle: rotacion.value, child: child);
+      },
+    );
+  }
+}
+
+class _Rectangulo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(color: Colors.blue),
+    );
+  }
+}
