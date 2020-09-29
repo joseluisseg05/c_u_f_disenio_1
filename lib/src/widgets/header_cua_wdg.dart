bc2a78b67685c4f3e02e801473782d9a4834ce2a
Jose Luis Garcia M <jl.garcia@ardana.com.mx>
2020-09-28 19:44:03 -0500

    Custom Painter Pico

--- lib/src/widgets/header_cua_wdg.dart
+++ lib/src/widgets/header_cua_wdg.dart
@@ -102,3 +102,44 @@ class _TrianguloPainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderPico extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Colors.teal,
+      child: CustomPaint(
+        painter: _PicoPainter(),
+      ),
+    );
+  }
+}
+
+class _PicoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    //propiedades
+    paint.color = Colors.teal;
+    //paint.style = PaintingStyle.stroke; //dibuja la linea
+    paint.style = PaintingStyle.fill; //rellena
+    paint.strokeWidth = 5.0;
+
+    final path = Path();
+    //dibujar
+    path.lineTo(0, size.height * 0.25);
+    path.lineTo(size.width * 0.5, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
