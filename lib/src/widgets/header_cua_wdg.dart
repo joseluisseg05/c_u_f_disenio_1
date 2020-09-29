e8165a0f89237b0191342d07492410295916e07a
Jose Luis Garcia M <jl.garcia@ardana.com.mx>
2020-09-28 19:57:27 -0500

    Custom Painter Curva

--- lib/src/widgets/header_cua_wdg.dart
+++ lib/src/widgets/header_cua_wdg.dart
@@ -143,3 +143,48 @@ class _PicoPainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderCurvo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Colors.teal,
+      child: CustomPaint(
+        painter: _CurvoPainter(),
+      ),
+    );
+  }
+}
+
+class _CurvoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    //propiedades
+    paint.color = Colors.teal;
+    //paint.style = PaintingStyle.stroke; //dibuja la linea
+    paint.style = PaintingStyle.fill; //rellena
+    paint.strokeWidth = 5.0;
+
+    final path = Path();
+    //dibujar
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(
+      size.width * 0.5, // estas dos lineas es el punto donde realiza la curva
+      size.height * 0.4,
+      size.width, //estas dos lineas corresponde a donde termina la curva
+      size.height * 0.25,
+    );
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
